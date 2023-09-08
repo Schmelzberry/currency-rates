4a17a34dd09a48892079c7597e8c5ef748417b1b
Schmelzberry <jelsberry96@gmail.com>
2023-09-08 12:53:30 -0700

    fetch api call WIP

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,3 +1,19 @@
 // need to use for referring to API key :
 //const url = https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD 
-${process.env.API_KEY}
+
+// BUSINESS LOGIC //
+export default class CurrencyExchange {
+  static async getAustralia(AUD) {
+    try {
+      const APIresponse = await fetch ('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD');
+      const JSONresponse = await APIresponse.json();
+      if (!APIresponse.ok) {
+        const errorMessage = `${APIresponse.status} ${APIresponse.statusText} ${JSONresponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return JSONresponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
